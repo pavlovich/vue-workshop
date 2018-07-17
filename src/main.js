f584d8aaff96f23ddad48adff56adb312dbecea0
Peter Pavlovich <pavlovich@gmail.com>
2018-07-16 23:52:36 -0600

    Add Vuex for centralized state management.

--- src/main.js
+++ src/main.js
@@ -2,9 +2,11 @@ import '@babel/polyfill'
 import Vue from 'vue'
 import './plugins/vuetify'
 import App from './App.vue'
+import store from './store'
 
 Vue.config.productionTip = false;
 
 new Vue({
+  store,
   render: h => h(App)
 }).$mount('#app');
